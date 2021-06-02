e20501c95a5fb44bdb8e8beb1c97166bf4fe98b5
Philip Walton <philip@philipwalton.com>
2021-06-01 17:20:42 -0700

    Add more descriptive comments

--- src/getCLS.ts
+++ src/getCLS.ts
@@ -61,8 +61,9 @@ export const getCLS = (onReport: ReportHandler, reportAllChanges?: boolean) => {
       const firstSessionEntry = sessionEntries[0];
       const lastSessionEntry = sessionEntries[sessionEntries.length - 1];
 
-      // If the entry is part of the current session, add it.
-      // Otherwise, start a new session.
+      // If the entry occurred less than 1 second after the previous entry and
+      // less than 5 seconds after the first entry in the session, include the
+      // entry in the current session. Otherwise, start a new session.
       if (sessionValue &&
           entry.startTime - lastSessionEntry.startTime < 1000 &&
           entry.startTime - firstSessionEntry.startTime < 5000) {
@@ -73,6 +74,8 @@ export const getCLS = (onReport: ReportHandler, reportAllChanges?: boolean) => {
         sessionEntries = [entry];
       }
 
+      // If the current session value is larger than the current CLS value,
+      // update CLS and the entries contributing to it.
       if (sessionValue > metric.value) {
         metric.value = sessionValue;
         metric.entries = sessionEntries;
