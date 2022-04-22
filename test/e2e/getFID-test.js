9d240c8823d58eea85ce6c209dbb66343b2bc88b
Philip Walton <philip@philipwalton.com>
2022-04-22 15:01:59 -0700

    Update FID tests to handle recent Chrome change

--- test/e2e/getFID-test.js
+++ test/e2e/getFID-test.js
@@ -50,7 +50,7 @@ describe('getFID()', async function() {
     assert(fid.id.match(/^v2-\d+-\d+$/));
     assert.strictEqual(fid.name, 'FID');
     assert.strictEqual(fid.value, fid.delta);
-    assert.strictEqual(fid.entries[0].name, 'mousedown');
+    assert(fid.entries[0].name.match(/(mouse|pointer)down/));
   });
 
   it('does not report if the browser does not support FID and the polyfill is not used', async function() {
@@ -100,7 +100,7 @@ describe('getFID()', async function() {
     assert(fid.id.match(/^v2-\d+-\d+$/));
     assert.strictEqual(fid.name, 'FID');
     assert.strictEqual(fid.value, fid.delta);
-    assert.strictEqual(fid.entries[0].name, 'mousedown');
+    assert(fid.entries[0].name.match(/(mouse|pointer)down/));
     if (browserSupportsFID) {
       assert('duration' in fid.entries[0]);
     } else {
@@ -165,7 +165,7 @@ describe('getFID()', async function() {
     assert(fid1.id.match(/^v2-\d+-\d+$/));
     assert.strictEqual(fid1.name, 'FID');
     assert.strictEqual(fid1.value, fid1.delta);
-    assert.strictEqual(fid1.entries[0].name, 'mousedown');
+    assert(fid1.entries[0].name.match(/(mouse|pointer)down/));
 
     await clearBeacons();
     await stubForwardBack();
@@ -181,7 +181,7 @@ describe('getFID()', async function() {
     assert(fid1.id !== fid2.id);
     assert.strictEqual(fid2.name, 'FID');
     assert.strictEqual(fid2.value, fid2.delta);
-    assert.strictEqual(fid2.entries[0].name, 'mousedown');
+    assert(fid2.entries[0].name.match(/(mouse|pointer)down/));
   });
 });
 
