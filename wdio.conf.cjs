ed7ae9e59d9dbb499435fe3dbbc6b0b402233d9e
Barry Pollard <barrypollard@google.com>
2024-04-05 10:32:40 +0100

    Give up, leave to just unit tests

--- wdio.conf.cjs
+++ wdio.conf.cjs
@@ -82,7 +82,6 @@ module.exports.config = {
     if (browserName === 'chrome') {
       capability['goog:chromeOptions'] = {
         excludeSwitches: ['enable-automation'],
-        args: ['headless', 'disable-gpu', 'remote-debugging-pipe'], // Need for GitHub Actions
         // Uncomment to test on Chrome Canary.
         // binary: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
       };
