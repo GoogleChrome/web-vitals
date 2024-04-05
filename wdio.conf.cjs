7296d49108e335e7d8baf21557051a24d7fc6e0c
Barry Pollard <barrypollard@google.com>
2024-04-05 09:22:20 +0100

    Retry Chrome tests

--- wdio.conf.cjs
+++ wdio.conf.cjs
@@ -82,6 +82,7 @@ module.exports.config = {
     if (browserName === 'chrome') {
       capability['goog:chromeOptions'] = {
         excludeSwitches: ['enable-automation'],
+        args: ['remote-debugging-pipe'],
         // Uncomment to test on Chrome Canary.
         // binary: '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
       };
