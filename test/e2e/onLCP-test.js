27f5d87205e852d5b98cd7bb10304800a96c0fe6
Barry Pollard <barrypollard@google.com>
2024-05-14 08:41:18 -0700

    Only apply Firefox workarounds in GitHub Actions (#481)
    * Only apply Firefox workarounds in GitHub Actions
    
    * Fix test
    
    * Add override console logging

--- test/e2e/onLCP-test.js
+++ test/e2e/onLCP-test.js
@@ -27,6 +27,7 @@ import {stubVisibilityChange} from '../utils/stubVisibilityChange.js';
 const originalStrictEqual = assert.strictEqual;
 assert.strictEqual = function (actual, expected, message) {
   if (
+    process.env.GITHUB_ACTIONS &&
     browser.capabilities.browserName === 'firefox' &&
     (expected === 'good' || expected === 'needs-improvement') &&
     actual !== expected
@@ -717,7 +718,15 @@ const assertFullReportsAreCorrect = (beacons) => {
 
   // Temp fix to address Firefox flakiness.
   // See https://github.com/GoogleChrome/web-vitals/issues/472
-  if (browser.capabilities.browserName !== 'firefox') {
+  if (
+    process.env.GITHUB_ACTIONS &&
+    browser.capabilities.browserName === 'firefox' &&
+    lcp1.value >= 500
+  ) {
+    console.log(
+      `Override assert for Firefox (actual: ${lcp1.value}, expected: < 500)`,
+    );
+  } else {
     assert(lcp1.value < 500); // Less than the image load delay.
   }
   assert(lcp1.id.match(/^v4-\d+-\d+$/));
