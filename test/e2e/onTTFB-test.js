5c522a993f4bd8dc33f28797247004aae03ad5aa
Barry Pollard <barrypollard@google.com>
2025-04-15 15:23:00 +0100

    Add 103 Early Hints test

--- test/e2e/onTTFB-test.js
+++ test/e2e/onTTFB-test.js
@@ -398,6 +398,35 @@ describe('onTTFB()', async function () {
       assert.strictEqual(ttfb.attribution.requestDuration, 0);
       assert.strictEqual(ttfb.attribution.navigationEntry, undefined);
     });
+
+    it('reports the correct value for Early Hints', async function () {
+      await navigateTo(
+        // '/test/ttfb?responseStart=10&earlyHintsDelay=50&attribution=1',
+        '/test/ttfb?earlyHintsDelay=50&attribution=1',
+      );
+
+      const ttfb = await getTTFBBeacon();
+
+      if ('finalResponseHeadersStart' in ttfb.attribution.navigationEntry) {
+        assert.strictEqual(
+          ttfb.value,
+          ttfb.attribution.navigationEntry.responseStart,
+        );
+        assert.strictEqual(
+          ttfb.value,
+          ttfb.attribution.navigationEntry.firstInterimResponseStart,
+        );
+        assert(
+          ttfb.value <
+            ttfb.attribution.navigationEntry.finalResponseHeadersStart,
+        );
+      } else {
+        assert.strictEqual(
+          ttfb.value,
+          ttfb.attribution.navigationEntry.responseStart,
+        );
+      }
+    });
   });
 });
 
