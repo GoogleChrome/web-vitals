844ad1a0c53af507918b8b236bb7eb77e894fae1
Barry Pollard <barrypollard@google.com>
2026-01-10 11:59:51 +0000

    Another fix

--- test/e2e/onLCP-test.js
+++ test/e2e/onLCP-test.js
@@ -730,8 +730,8 @@ describe('onLCP()', async function () {
       // TODO - this is flakey in Safari. Need to find out why.
       if (browser.capabilities.browserName !== 'Safari') {
         assert(lcp.attribution.url.endsWith('/test/img/square.png?delay=500'));
+        assert.equal(lcp.attribution.target, 'html>body>main>p>img.bar.foo');
       }
-      assert.equal(lcp.attribution.target, 'html>body>main>p>img.bar.foo');
 
       // Specifically check that resourceLoadDelay falls back to `startTime`.
       assert.equal(
