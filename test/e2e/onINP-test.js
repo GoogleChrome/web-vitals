3d9f4aefdacec434e226645289f081d276852cfe
Barry Pollard <barrypollard@google.com>
2026-07-20 19:14:05 +0100

    Log console errors

--- test/e2e/onINP-test.js
+++ test/e2e/onINP-test.js
@@ -783,8 +783,8 @@ describe('onINP()', async function () {
     });
     console.log(softNavs);
 
-    // Wait a bit to allow the entries to report
-    await browser.pause(1000);
+    const logs = await browser.getLogs('browser');
+    console.log(logs);
 
     await beaconCountIs(1);
 
